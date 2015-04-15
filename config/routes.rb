f1da3aa5d4924bbfdd2296a719977640e64e438d
Evgeny Lapin <lev@openteam.ru>
2015-04-15 17:21:33 +0600

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Pnms::Application.routes.draw do
 
   resources :reports
 
-  get '/ru/registratsiya' => 'claims#index'
-  get '/ru/registratsiya/new' => 'claims#new'
+  get '/ru/registratsiya' => 'claims#new'
   post '/ru/registratsiya' => 'claims#create'
   get '/ru/registratsiya/done' => 'claims#done'
 
-  get '/en/registration' => 'claims#index'
-  get '/en/registration/new' => 'claims#new'
+  get '/en/registration' => 'claims#new'
   post '/en/registration' => 'claims#create'
   get '/en/registration/done' => 'claims#done'
 
