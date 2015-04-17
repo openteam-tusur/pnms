bdbba67b7388b132f4b4ba4d1c528f1bc6706106
Evgeny Lapin <lev@openteam.ru>
2015-04-17 17:20:48 +0600

    redirect to root with message instead of forbidden error

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -2,4 +2,7 @@ class Manage::ApplicationController < ApplicationController
 
   load_and_authorize_resource
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => I18n.t('access.not_authorized')
+  end
 end
